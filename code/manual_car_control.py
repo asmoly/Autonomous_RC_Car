e9eb24b0f51856431c6c6f76753020ec5fb77693
sashasmol2007 <asmoly@outlook.com>
2023-06-18 20:43:17 -0700

    Update manual_car_control.py

--- code/manual_car_control.py
+++ code/manual_car_control.py
@@ -3,9 +3,10 @@ import car_controlls
 
 car_controller = car_controlls.Car_Controller(33, 32)
 
-if keyboard.is_pressed("a"):
-    car_controller.set_steering(-0.5)
-elif keyboard.is_pressed("d"):
-    car_controller.set_steering(0.5)
-else:
-    car_controller.set_steering(0)
+while True:
+    if keyboard.is_pressed("a"):
+        car_controller.set_steering(-0.5)
+    elif keyboard.is_pressed("d"):
+        car_controller.set_steering(0.5)
+    else:
+        car_controller.set_steering(0)
