fa969dbe330cb6fe13666f18013bce53bd0740c4
sashasmol2007 <asmoly@outlook.com>
2023-06-18 20:26:48 -0700

    Create manual_car_control.py

---
+++ code/manual_car_control.py
@@ -0,0 +1,11 @@
+import keyboard
+import car_controlls
+
+car_controller = car_controlls.Car_Controller(33, 32)
+
+if keyboard.is_pressed("a"):
+    car_controller.set_steering(-0.5)
+elif keyboard.is_pressed("d"):
+    car_controller.set_steering(0.5)
+else:
+    car_controller.set_steering(0)
