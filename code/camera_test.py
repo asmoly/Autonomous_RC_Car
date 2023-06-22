4f46cb0c55f7e2dd725271a511fd00fcb17bf238
Alexander <asmoly@outlook.com>
2023-06-22 14:55:44 -0700

    added camera test file

---
+++ code/camera_test.py
@@ -0,0 +1,40 @@
+import pyzed.sl as sl
+import cv2
+
+def main():
+    # Create a Camera object
+    zed = sl.Camera()
+
+    # Create a InitParameters object and set configuration parameters
+    init_params = sl.InitParameters()
+    init_params.camera_resolution = sl.RESOLUTION.HD1080  # Use HD1080 video mode
+    init_params.camera_fps = 30  # Set fps at 30
+
+    # Open the camera
+    err = zed.open(init_params)
+    if err != sl.ERROR_CODE.SUCCESS:
+        exit(1)
+
+    # Capture 50 frames and stop
+    image = sl.Mat()
+    runtime_parameters = sl.RuntimeParameters()
+    while True:
+        # Grab an image, a RuntimeParameters object must be given to grab()
+        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
+            # A new image is available if grab() returns SUCCESS
+            zed.retrieve_image(image, sl.VIEW.LEFT)
+            timestamp = zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)  # Get the timestamp at the time the image was captured
+            print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(image.get_width(), image.get_height(),
+                  timestamp.get_milliseconds()))
+            i = i + 1
+
+            cv2.imshow("Camera Test", image.get_data())
+            cv2.waitKey(1)
+
+        
+
+    # Close the camera
+    zed.close()
+
+if __name__ == "__main__":
+    main()
