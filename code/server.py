7004f024eb5d92e320cda175a2f3b589c4c69280
Alexander <asmoly@outlook.com>
2023-06-24 14:05:00 -0700

    changed server

--- code/server.py
+++ code/server.py
@@ -1,9 +1,9 @@
 import socket
  
-UDP_IP = "73.254.187.38"
+UDP_IP = "10.42.0.120"
 UDP_PORT = 5005
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 while True:
-    sock.sendto(b"testing", (UDP_IP, UDP_PORT))
+    sock.sendto(b"hello", (UDP_IP, UDP_PORT))
