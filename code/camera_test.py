49db21d35161292876b1037cb009a34ac4046726
Alexander Smolyanskiy <asmoly@outlook.com>
2023-06-23 18:57:39 -0700

    Update camera_test.py

--- code/camera_test.py
+++ code/camera_test.py
@@ -1,40 +1,47 @@
 import pyzed.sl as sl
 import cv2
+import keyboard
 
 def main():
-    # Create a Camera object
     zed = sl.Camera()
 
-    # Create a InitParameters object and set configuration parameters
     init_params = sl.InitParameters()
-    init_params.camera_resolution = sl.RESOLUTION.HD1080  # Use HD1080 video mode
-    init_params.camera_fps = 30  # Set fps at 30
+    init_params.camera_resolution = sl.RESOLUTION.HD1080
+    init_params.camera_fps = 30
 
-    # Open the camera
+    init_params.sdk_verbose = True # Enable verbose logging
+    init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE # Set the depth mode to performance (fastest)
+    init_params.coordinate_units = sl.UNIT.MILLIMETER  # Use millimeter units
+
     err = zed.open(init_params)
     if err != sl.ERROR_CODE.SUCCESS:
         exit(1)
 
-    # Capture 50 frames and stop
-    image = sl.Mat()
-    runtime_parameters = sl.RuntimeParameters()
-    while True:
-        # Grab an image, a RuntimeParameters object must be given to grab()
-        if zed.grab(runtime_parameters) == sl.ERROR_CODE.SUCCESS:
-            # A new image is available if grab() returns SUCCESS
-            zed.retrieve_image(image, sl.VIEW.LEFT)
-            timestamp = zed.get_timestamp(sl.TIME_REFERENCE.CURRENT)  # Get the timestamp at the time the image was captured
-            print("Image resolution: {0} x {1} || Image timestamp: {2}\n".format(image.get_width(), image.get_height(),
-                  timestamp.get_milliseconds()))
-            i = i + 1
+    print("Opened Camera")
 
-            cv2.imshow("Camera Test", image.get_data())
+    image = sl.Mat()
+    depth = sl.Mat()
+    depth_image = sl.Mat()
+
+    while keyboard.is_pressed("a") == False:
+        if zed.grab(sl.RuntimeParameters()) == sl.ERROR_CODE.SUCCESS:
+            zed.retrieve_image(image, sl.VIEW.LEFT)
+
+            image_as_array = image.get_data()
+            image_as_array = cv2.putText(image_as_array, f"{image.get_width()}x{image.get_height()}", (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
+
+            zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
+            depth_as_array = depth.get_data()
+
+            zed.retrieve_image(depth_image, sl.VIEW.DEPTH)
+            depth_image_as_array = depth_image.get_data()
+
+            cv2.imshow("Video", depth_image_as_array)
+
             cv2.waitKey(1)
 
-        
-
-    # Close the camera
     zed.close()
+    print("Closed Camera")
 
 if __name__ == "__main__":
     main()
