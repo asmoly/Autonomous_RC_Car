2bb3a49aa53f6781cc3538a2fb23ae38c72cf46e
Alexander Smolyanskiy <asmoly@outlook.com>
2024-04-16 19:27:19 -0700

    updated car controller to work with jetson

--- code/controller/Car_Controller.py
+++ code/controller/Car_Controller.py
@@ -1,8 +1,14 @@
 import serial
 
 class Car_Controller:
-    def __init__(self, port="COM3", baudrate=115200) -> None:
-        self.arduino = serial.Serial(port=port, baudrate=baudrate) 
+    def __init__(self, port="/dev/ttyACM", baudrate=115200) -> None:
+        port_counter = 0
+        while port_counter <= 4:
+            try:
+                self.arduino = serial.Serial(port=port, baudrate=baudrate)
+                break
+            except:
+                port_counter += 1 
 
     def set_speed_and_steering(self, target_speed, target_steering): # speed is value from 0 - 1, steering is value from -1 - 1
         target_speed_translated = int(target_speed*100) # convert to sclae of 0 - 100 for arduino
