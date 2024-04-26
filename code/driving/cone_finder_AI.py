cd9e9f7c9188c4cc553e347dd8be4aa5aebad594
asmoly <asmoly@outlook.com>
2024-04-25 20:30:34 -0700

    added pid control to driving program

--- code/driving/cone_finder_AI.py
+++ code/driving/cone_finder_AI.py
@@ -13,7 +13,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 from torch.utils.data import DataLoader, Dataset
 
-from torch.utils.tensorboard import SummaryWriter
 from PIL import Image
 
 # Data settings
@@ -41,37 +40,6 @@ def convert_to_tensor(image):
 
 def clamp(value, minValue, maxValue):
     return max(min(maxValue, value), minValue)
-
-# def draw_labels(image, result):    
-#     for i in range(0, MAX_NUM_OF_FACES):
-#         image = cv2.rectangle(image, (int(result[0 + i*4]), int(result[1 + i*4])), (int(result[0 + i*4]) + int(result[2 + i*4]), int(result[1 + i*4]) + int(result[3 + i*4])), (0, 255, 0), 2)
-
-#     return image
-
-class FacesDataset(Dataset):
-    def __init__(self, path_to_targets, path_to_images, path_to_dataset_length, imageTransform=None):
-        self.imageTransform = imageTransform
-        self.path_to_images = path_to_images
-        self.path_to_targets = path_to_targets
-
-        self.dataset_length = pickle.load(open(path_to_dataset_length, "rb"))
-        print(self.dataset_length)
-
-    def __len__(self):
-        return self.dataset_length
-
-    def __getitem__(self, idx):
-        #image = cv2.imread(f"{self.path_to_images}/image_{idx}.png")
-        image = Image.open(f"{self.path_to_images}/image_{idx}.png")
-        image_as_tensor = self.imageTransform(image)
-
-        target = cv2.imread(f"{self.path_to_targets}/target_{idx}.png", cv2.IMREAD_GRAYSCALE)
-        target = cv2.resize(target, (0,0), fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST) 
-        target = remap_target(target)
-        target = torch.Tensor(target)
-        target = target.type(torch.LongTensor) # Casting to int
-
-        return image_as_tensor, target
 
 class FaceNet(nn.Module):
     def __init__(self):
@@ -147,101 +115,3 @@ def load_model(path):
 
     return model
             
-# start_epoch = 1 at the very begining!
-def train(device, start_epoch, n_epochs, path_to_targets, path_to_images, path_to_val_targets, path_to_val_images, path_to_data_length, path_to_val_data_length, path_to_logs, path_to_model = None):
-    writer = SummaryWriter(log_dir=path_to_logs)
-
-    data_loader = DataLoader(FacesDataset(imageTransform=dataTransformations, path_to_targets=path_to_targets, path_to_images=path_to_images, path_to_dataset_length=path_to_data_length), batch_size=16, shuffle=True)
-    validation_data_loader = DataLoader(FacesDataset(imageTransform=dataTransformations, path_to_targets=path_to_val_targets, path_to_images=path_to_val_images, path_to_dataset_length=path_to_val_data_length), batch_size=16, shuffle=True)
-
-    model = FaceNet().to(device)
-    if path_to_model != None :
-        model = load_model(path_to_model).to(device)
-
-    optimizer = optim.AdamW(model.parameters(), lr=1e-4)
-
-    startTime = time()
-    idx_counter = 0
-    for epoch_index in range (start_epoch, start_epoch + n_epochs):
-        print(f"Epoch {epoch_index}, lr {optimizer.param_groups[0]['lr']}")
-
-        loss_list = []
-        for images, targets in data_loader:
-            batchImages = images.to(device)
-            batchTargets = targets.to(device)
-
-            optimizer.zero_grad()
-
-            batchOuput = model(batchImages)
-            loss_ce = nn.CrossEntropyLoss(reduction="mean")(batchOuput, batchTargets)
-            total_loss = WEIGHT*loss_ce
-            loss_list.append(total_loss)
-
-            total_loss.backward()
-            optimizer.step()
-
-            if idx_counter % 20 == 0:
-                # Logging
-                image_to_log = images[0].permute(1, 2, 0).cpu().detach().numpy()
-                image_to_log *= 255.0   # de-normalize
-                image_to_log = image_to_log.astype(np.uint8).copy()
-
-                writer.add_scalar("Loss_ce", loss_ce, idx_counter)
-                
-                output = torch.argmax(batchOuput[0], dim=0)
-                writer.add_image("image", transforms.ToTensor()(image_to_log), idx_counter)
-                writer.add_image("target_mask", targets[0].cpu()/2.0, idx_counter, dataformats="HW")
-                writer.add_image("result", output.cpu()/2, idx_counter, dataformats="HW")
-                writer.flush()
-            
-            idx_counter += 1
-            print("Epoch: ", epoch_index, "Idx", idx_counter, " Total Loss: ", total_loss.item())
-
-        average_loss = 0
-        for loss in loss_list:
-            average_loss += loss
-        average_loss = average_loss/len(loss_list)
-        writer.add_scalar("Loss_ce_Per_Epoch", average_loss, epoch_index)
-        writer.flush()
-
-        loss_list = []
-        for images, targets in validation_data_loader:
-            batchImages = images.to(device)
-            batchTargets = targets.to(device)
-
-            with torch.no_grad():
-                batchOuput = model(batchImages)
-                loss_ce = nn.CrossEntropyLoss(reduction="mean")(batchOuput, batchTargets)
-                total_loss = WEIGHT*loss_ce
-                loss_list.append(total_loss)
-
-        average_loss = 0
-        for loss in loss_list:
-            average_loss += loss
-        average_loss = average_loss/len(loss_list)
-        writer.add_scalar("Validation_Loss_ce_Per_Epoch", average_loss, epoch_index)
-        writer.flush()
-
-        # Save model checkpoint every 5th epoch
-        #if epoch_index % 5 == 0:
-        torch.save(model.state_dict(), os.path.join(path_to_logs, "conenet_{}.pt".format(epoch_index))) 
-
-    writer.close()
-    print("Training Time: ", (time() - startTime)/60)
-
-
-def main():
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    
-    train(device, start_epoch = 0, n_epochs = 200,
-          path_to_targets="dataset/targets",
-          path_to_images="dataset/images",
-          path_to_logs="logs",
-          path_to_model = None,
-          path_to_val_targets="dataset/validation_targets",
-          path_to_val_images="dataset/validation_images",
-          path_to_data_length="dataset/image_count",
-          path_to_val_data_length="dataset/validation_image_count")
-
-if __name__ == "__main__":
-    main()
--- code/driving/main.py
+++ code/driving/main.py
@@ -4,16 +4,26 @@ import numpy as np
 import pyzed.sl as sl
 import torchvision.transforms.functional as fn
 import torch
+from flask import Flask, render_template, Response
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as f
 from PIL import Image
 import math
-from cone_finder_AI import *
+
+import PI_Controller
+import Car_Controller
+from cone_finder_AI import load_model, clamp
 
 SESSION = 3
 IMAGE_WIDTH = 672
+IMAGE_HEIGHT = 376
 VGA_FOCAL_LENGTH = 367
+
+app = Flask(__name__)
+
+car_controller = Car_Controller.Car_Controller()
+pi_controller = PI_Controller.PI(0.1, 0.2)
 
 cam = sl.Camera()
 
@@ -34,45 +44,62 @@ image = sl.Mat()
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = load_model("conenet_30.pt").to(device)
 
-image_counter = 0
-while True:
-    if cam.grab(sl.RuntimeParameters()) == sl.ERROR_CODE.SUCCESS:
-        cam.retrieve_image(image, sl.VIEW.LEFT)
-    
-    image_as_array = image.get_data()
+def generate_frames():
+    while True:
+        if cam.grab(sl.RuntimeParameters()) == sl.ERROR_CODE.SUCCESS:
+            cam.retrieve_image(image, sl.VIEW.LEFT)
+        
+        image_as_array = image.get_data()
 
-    image_as_array_rgb = cv2.cvtColor(image_as_array[:, :, 0:3], cv2.COLOR_BGR2RGB)
-    PIL_image = Image.fromarray(image_as_array_rgb)
-    image_tensor = transforms.ToTensor()(PIL_image)
+        image_as_array_rgb = cv2.cvtColor(image_as_array[:, :, 0:3], cv2.COLOR_BGR2RGB)
+        PIL_image = Image.fromarray(image_as_array_rgb)
+        image_tensor = transforms.ToTensor()(PIL_image)
 
-    with torch.no_grad():
-        model_output = model(image_tensor.to(device))
-        model_output = model_output.softmax(dim=0)
+        with torch.no_grad():
+            model_output = model(image_tensor.to(device))
+            model_output = model_output.softmax(dim=0)
 
-        result_mask = torch.argmax(model_output, dim=0).cpu().detach().numpy().astype(np.uint8)
-        result_mask = cv2.resize(result_mask, (0,0), fx=2.0, fy=2.0, interpolation=cv2.INTER_NEAREST)
+            result_mask = torch.argmax(model_output, dim=0).cpu().detach().numpy().astype(np.uint8)
+            result_mask = cv2.resize(result_mask, (0,0), fx=2.0, fy=2.0, interpolation=cv2.INTER_NEAREST)
 
-        closest_cones = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0]]
+            closest_cones = [[0, 0, int(IMAGE_HEIGHT/2), 0, 0], [0, IMAGE_WIDTH - 1, int(IMAGE_HEIGHT/2), 0, 0]] # green, orange
 
-        contours, hierarchy = cv2.findContours(result_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for i in range(0, len(contours)):
-            if cv2.contourArea(contours[i]) > 20: # Minimum number of pixels in countour
-                x, y, w, h = cv2.boundingRect(contours[i])
-                x, y, w, h = x, y, w, h
+            contours, hierarchy = cv2.findContours(result_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            for i in range(0, len(contours)):
+                if cv2.contourArea(contours[i]) > 300: # Minimum number of pixels in countour
+                    x, y, w, h = cv2.boundingRect(contours[i])
+                    x, y, w, h = x, y, w, h
 
-                if cv2.contourArea(contours[i]) > closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1][0]:
-                    closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1] = [cv2.contourArea(contours[i]), x, y, w, h]
+                    if cv2.contourArea(contours[i]) > closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1][0] and cv2.contourArea(contours[i]) < 10000:
+                        closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1] = [cv2.contourArea(contours[i]), x, y, w, h]
 
-        center_point = [((closest_cones[0][1] + closest_cones[0][3]/2) + (closest_cones[1][1] + closest_cones[1][3]/2))/2, ((closest_cones[0][2] + closest_cones[0][4]/2) + (closest_cones[1][2] + closest_cones[1][4]/2))/2]
-        angle_to_center_point = math.atan2(center_point[0] - IMAGE_WIDTH/2, VGA_FOCAL_LENGTH)
+            center_point = [((closest_cones[0][1] + closest_cones[0][3]/2) + (closest_cones[1][1] + closest_cones[1][3]/2))/2, ((closest_cones[0][2] + closest_cones[0][4]/2) + (closest_cones[1][2] + closest_cones[1][4]/2))/2]
+            angle_to_center_point = math.atan2(center_point[0] - IMAGE_WIDTH/2, VGA_FOCAL_LENGTH)
 
-        cv2.rectangle(image_as_array, (closest_cones[0][1], closest_cones[0][2]), (closest_cones[0][1] + closest_cones[0][3], closest_cones[0][2] + closest_cones[0][4]), (0, 0, 255), 2)
-        cv2.rectangle(image_as_array, (closest_cones[1][1], closest_cones[1][2]), (closest_cones[1][1] + closest_cones[1][3], closest_cones[1][2] + closest_cones[1][4]), (0, 0, 255), 2)
+            steering = pi_controller.calculate_error(angle_to_center_point)
+            steering = clamp(steering, -1.0, 1.0)*-1.0
+            #print(steering)
+            car_controller.set_speed_and_steering(1, steering, translate_values=True)
 
-        cv2.line(image_as_array, (int(closest_cones[0][1] + closest_cones[0][3]/2), int(closest_cones[0][2] + closest_cones[0][4]/2)), (int(closest_cones[1][1] + closest_cones[1][3]/2), int(closest_cones[1][2] + closest_cones[1][4]/2)), (0, 0, 255), 2)
-        cv2.line(image_as_array, (int(IMAGE_WIDTH/2), 0), (int(IMAGE_WIDTH/2), 375), (0, 255, 0), 2)
-        cv2.circle(image_as_array, (int(center_point[0]), int(center_point[1])), 2, (0, 255, 0), 3)
+            cv2.rectangle(image_as_array, (closest_cones[0][1], closest_cones[0][2]), (closest_cones[0][1] + closest_cones[0][3], closest_cones[0][2] + closest_cones[0][4]), (0, 0, 255), 2)
+            cv2.rectangle(image_as_array, (closest_cones[1][1], closest_cones[1][2]), (closest_cones[1][1] + closest_cones[1][3], closest_cones[1][2] + closest_cones[1][4]), (0, 0, 255), 2)
 
-    cv2.imshow("drive debugger", image_as_array)
-    cv2.waitKey(20)
-    image_counter += 1
+            cv2.line(image_as_array, (int(closest_cones[0][1] + closest_cones[0][3]/2), int(closest_cones[0][2] + closest_cones[0][4]/2)), (int(closest_cones[1][1] + closest_cones[1][3]/2), int(closest_cones[1][2] + closest_cones[1][4]/2)), (0, 0, 255), 2)
+            cv2.line(image_as_array, (int(IMAGE_WIDTH/2), 0), (int(IMAGE_WIDTH/2), 375), (0, 255, 0), 2)
+            cv2.circle(image_as_array, (int(center_point[0]), int(center_point[1])), 2, (0, 255, 0), 3)
+
+        _, buffer = cv2.imencode('.jpg', image_as_array)
+        frame_bytes = buffer.tobytes()
+        yield(b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+        
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
