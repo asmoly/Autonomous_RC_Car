650571bbdb224c7ba59db915fad66803ed14d711
asmoly <asmoly@outlook.com>
2024-04-26 17:52:54 -0700

    cleaned up code

--- code/driving/PI_Controller.py
+++ code/driving/PI_Controller.py
@@ -1,20 +1,17 @@
-import time
-
 class PI:
-    def __init__(self, p, i) -> None:
-        self.p = p
-        self.i = i
+    def __init__(self, p_gain, i_gain) -> None:
+        self.p_gain = p_gain
+        self.i_gain = i_gain
         self.errors = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-        self.previous_time = 0
 
-    def calculate_error(self, error):
+    def compute_control(self, error):
         self.errors.pop(0)
         self.errors.append(error)
 
-        proportional = error*self.p
-
-        integral = 0
+        error_integral = 0.0
         for e in self.errors:
-            integral += e*self.i
+            error_integral += e
 
-        return proportional + integral
+        control = error*self.p_gain + error_integral*self.i_gain  # PI controller
+        return control
+    
--- code/driving/main.py
+++ code/driving/main.py
@@ -1,15 +1,15 @@
 import sys
+import math
 import cv2
 import numpy as np
-import pyzed.sl as sl
-import torchvision.transforms.functional as fn
 import torch
-from flask import Flask, render_template, Response
+import torchvision.transforms.functional as fn
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as f
+import pyzed.sl as sl
 from PIL import Image
-import math
+from flask import Flask, render_template, Response
 
 import PI_Controller
 import Car_Controller
@@ -18,88 +18,98 @@ from cone_finder_AI import load_model, clamp
 SESSION = 3
 IMAGE_WIDTH = 672
 IMAGE_HEIGHT = 376
-VGA_FOCAL_LENGTH = 367
+CAMERA_VGA_FOCAL_LENGTH = 367
 
-app = Flask(__name__)
+CONE_MIN_BLOB_AREA = 300 # in pixels
+CONE_MAX_HEIGHT_PERCENT = 1.0 # in percents
+LEFT_MARGIN_POINT = (0, 0)
+RIGHT_MARGIN_POINT = (IMAGE_WIDTH - 1, 0)
+
+P_G = 0.1   # PID's P term gain
+I_G = 0.2   # PID's I term gain
+
+streaming_app = Flask(__name__)
 
 car_controller = Car_Controller.Car_Controller()
-pi_controller = PI_Controller.PI(0.1, 0.2)
+pi_controller = PI_Controller.PI(P_G, I_G)
 
-cam = sl.Camera()
+camera = sl.Camera()
 
-init_params = sl.InitParameters()
-init_params.camera_resolution = sl.RESOLUTION.VGA
-init_params.camera_fps = 60
+camera_params = sl.InitParameters()
+camera_params.camera_resolution = sl.RESOLUTION.VGA
+camera_params.camera_fps = 60
+camera_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE # Set the depth mode to performance (fastest)
+camera_params.coordinate_units = sl.UNIT.MILLIMETER  # Use millimeter units
 
-init_params.depth_mode = sl.DEPTH_MODE.PERFORMANCE # Set the depth mode to performance (fastest)
-init_params.coordinate_units = sl.UNIT.MILLIMETER  # Use millimeter units
-
-err = cam.open(init_params)
+err = camera.open(camera_params)
 if err != sl.ERROR_CODE.SUCCESS:
-    print("Camera failed to open")
+    print("ERROR: Camera failed to open!")
     sys.exit()
-
-image = sl.Mat()
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 model = load_model("conenet_30.pt").to(device)
 
-def generate_frames():
-    while True:
-        if cam.grab(sl.RuntimeParameters()) == sl.ERROR_CODE.SUCCESS:
-            cam.retrieve_image(image, sl.VIEW.LEFT)
-        
-        image_as_array = image.get_data()
+frame = sl.Mat()
 
-        image_as_array_rgb = cv2.cvtColor(image_as_array[:, :, 0:3], cv2.COLOR_BGR2RGB)
-        PIL_image = Image.fromarray(image_as_array_rgb)
+def process_frames():
+
+    while True:
+        if camera.grab(sl.RuntimeParameters()) == sl.ERROR_CODE.SUCCESS:
+            camera.retrieve_image(frame, sl.VIEW.LEFT)
+        
+        frame_as_array = frame.get_data()
+        frame_as_array_rgb = cv2.cvtColor(frame_as_array[:, :, 0:3], cv2.COLOR_BGR2RGB)
+        PIL_image = Image.fromarray(frame_as_array_rgb)
         image_tensor = transforms.ToTensor()(PIL_image)
 
         with torch.no_grad():
             model_output = model(image_tensor.to(device))
             model_output = model_output.softmax(dim=0)
-
             result_mask = torch.argmax(model_output, dim=0).cpu().detach().numpy().astype(np.uint8)
-            result_mask = cv2.resize(result_mask, (0,0), fx=2.0, fy=2.0, interpolation=cv2.INTER_NEAREST)
+            result_mask = cv2.resize(result_mask, (0,0), fx=2.0, fy=2.0, interpolation=cv2.INTER_NEAREST)   # upscale 2x
 
-            closest_cones = [[0, 0, int(IMAGE_HEIGHT/2), 0, 0], [0, IMAGE_WIDTH - 1, int(IMAGE_HEIGHT/2), 0, 0]] # green, orange
+            closest_cones = [[0, LEFT_MARGIN_POINT[0], LEFT_MARGIN_POINT[1], 0, 0], [0, RIGHT_MARGIN_POINT[0], RIGHT_MARGIN_POINT[1], 0, 0]] # green, orange
 
-            contours, hierarchy = cv2.findContours(result_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+            contours, _ = cv2.findContours(result_mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
             for i in range(0, len(contours)):
-                if cv2.contourArea(contours[i]) > 300: # Minimum number of pixels in countour
+                blob_area = cv2.contourArea(contours[i])
+                if blob_area > CONE_MIN_BLOB_AREA:   # TODO: this is in pixels! Must be changed to percent of total frame pixels
                     x, y, w, h = cv2.boundingRect(contours[i])
-                    x, y, w, h = x, y, w, h
 
-                    if cv2.contourArea(contours[i]) > closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1][0] and cv2.contourArea(contours[i]) < 10000:
-                        closest_cones[result_mask[contours[i][0][0][1], contours[i][0][0][0]] - 1] = [cv2.contourArea(contours[i]), x, y, w, h]
+                    blob_class_id = result_mask[contours[i][0][0][1], contours[i][0][0][0]]  # 0 - background, 1 - green, 2 - red
+                    if blob_area > closest_cones[blob_class_id - 1][0] and h <= CONE_MAX_HEIGHT_PERCENT*IMAGE_HEIGHT:
+                        closest_cones[blob_class_id - 1] = [blob_area, x, y, w, h]
 
-            center_point = [((closest_cones[0][1] + closest_cones[0][3]/2) + (closest_cones[1][1] + closest_cones[1][3]/2))/2, ((closest_cones[0][2] + closest_cones[0][4]/2) + (closest_cones[1][2] + closest_cones[1][4]/2))/2]
-            angle_to_center_point = math.atan2(center_point[0] - IMAGE_WIDTH/2, VGA_FOCAL_LENGTH)
+            # Compute center point between nearest cones to drive to and then the heading angle to that point
+            left_cone_center = ((closest_cones[0][1] + closest_cones[0][3]/2), (closest_cones[0][2] + closest_cones[0][4]/2))
+            right_cone_center = ((closest_cones[1][1] + closest_cones[1][3]/2), (closest_cones[1][2] + closest_cones[1][4]/2))
+            center_point = [(left_cone_center[0] + right_cone_center[0])/2, (left_cone_center[1] + right_cone_center[1])/2]
+            angle_to_center_point = math.atan2(center_point[0] - IMAGE_WIDTH/2, CAMERA_VGA_FOCAL_LENGTH)
 
-            steering = pi_controller.calculate_error(angle_to_center_point)
+            # Calculate steering and send it to steering servo
+            steering = pi_controller.compute_control(angle_to_center_point)
             steering = clamp(steering, -1.0, 1.0)*-1.0
             #print(steering)
             car_controller.set_speed_and_steering(1, steering, translate_values=True)
 
-            cv2.rectangle(image_as_array, (closest_cones[0][1], closest_cones[0][2]), (closest_cones[0][1] + closest_cones[0][3], closest_cones[0][2] + closest_cones[0][4]), (0, 0, 255), 2)
-            cv2.rectangle(image_as_array, (closest_cones[1][1], closest_cones[1][2]), (closest_cones[1][1] + closest_cones[1][3], closest_cones[1][2] + closest_cones[1][4]), (0, 0, 255), 2)
+            cv2.rectangle(frame_as_array, (closest_cones[0][1], closest_cones[0][2]), (closest_cones[0][1] + closest_cones[0][3], closest_cones[0][2] + closest_cones[0][4]), (0, 0, 255), 2)
+            cv2.rectangle(frame_as_array, (closest_cones[1][1], closest_cones[1][2]), (closest_cones[1][1] + closest_cones[1][3], closest_cones[1][2] + closest_cones[1][4]), (0, 0, 255), 2)
+            cv2.line(frame_as_array, (int(left_cone_center[0]), int(left_cone_center[1])), (int(right_cone_center[0]), int(right_cone_center[1])), (0, 0, 255), 2)
+            cv2.line(frame_as_array, (int(IMAGE_WIDTH/2), 0), (int(IMAGE_WIDTH/2), 375), (0, 255, 0), 2)
+            cv2.circle(frame_as_array, (int(center_point[0]), int(center_point[1])), 2, (0, 255, 0), 3)
 
-            cv2.line(image_as_array, (int(closest_cones[0][1] + closest_cones[0][3]/2), int(closest_cones[0][2] + closest_cones[0][4]/2)), (int(closest_cones[1][1] + closest_cones[1][3]/2), int(closest_cones[1][2] + closest_cones[1][4]/2)), (0, 0, 255), 2)
-            cv2.line(image_as_array, (int(IMAGE_WIDTH/2), 0), (int(IMAGE_WIDTH/2), 375), (0, 255, 0), 2)
-            cv2.circle(image_as_array, (int(center_point[0]), int(center_point[1])), 2, (0, 255, 0), 3)
-
-        _, buffer = cv2.imencode('.jpg', image_as_array)
+        _, buffer = cv2.imencode('.jpg', frame_as_array)
         frame_bytes = buffer.tobytes()
         yield(b'--frame\r\n'
                 b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
         
-@app.route('/')
+@streaming_app.route('/')
 def index():
     return render_template("index.html")
 
-@app.route('/video_feed')
+@streaming_app.route('/video_feed')
 def video_feed():
-    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(process_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    streaming_app.run(host='0.0.0.0', port=5000)
